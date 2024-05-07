5458b10384b4a220236acfed5664928ba53fde3a
Wonki11 <wongi5011@daum.net>
2024-05-07 12:40:18 +0900

    예제문제 실습

---
+++ 자바_예제문제실습/src/자바_예제문제실습/Circle.java
@@ -0,0 +1,46 @@
+package 자바_예제문제실습;
+
+public class Circle {
+	public static final double PI = 3.14;
+	public double radius;
+	
+	
+
+	
+
+	public Circle(double d) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static double getPi() {
+		return PI;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public void draw() {
+		System.out.println("반지름" + radius + "cm인 원을 그립니다.");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		Circle 원 = new Circle(2.5);
+		원.draw();
+	}
+}
---
+++ 자바_예제문제실습/src/자바_예제문제실습/Main.java
@@ -0,0 +1,28 @@
+package 자바_예제문제실습;
+
+import java.util.Scanner;
+
+/*
+사용자에게 문자열을 입력 받아 문자열의 길이를 출력하기
+입력할 문자열이 finish 면 프로그램을 종료하게 하길 원함
+
+ * */
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("문자열을 입력해주세요");
+			String str = sc.nextLine();
+			if(str.equals("finish")) {
+				break;
+			} else {
+				System.out.println(str.length() + " 글자 입니다.");
+				
+			}
+		}
+		System.out.println("프로그램을 종료합니다.");
+	}
+	
+
+}
