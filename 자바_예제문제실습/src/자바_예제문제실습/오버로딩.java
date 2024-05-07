74fc7b3b94a6b46298aa35fc6fe28097e7c489b8
Wonki11 <wongi5011@daum.net>
2024-05-07 17:02:24 +0900

    오버로딩

---
+++ 자바_예제문제실습/src/자바_예제문제실습/오버로딩.java
@@ -0,0 +1,28 @@
+package 자바_예제문제실습;
+
+public class 오버로딩 {
+	
+	public void test() {}
+		
+	public void test(String str) {}
+	
+	public void test(int i) {}
+	
+	public void test(String s , String a) {}
+	
+	private void test(int a , int b) {}
+	
+	public void test(char a , char c) {}
+	
+	void test(char ch) {}
+	
+	public void test(boolean a , Integer s , String c) {}
+	public void test(boolean b) {}
+	public void test(short s , long a , float x) {}
+	public void test(short c) {}
+	
+	public void test(int a, String b, Integer s) {}
+	
+	
+	
+}
